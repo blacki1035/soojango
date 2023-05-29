e098ae575e6f2f68fed278b9554029e671603dc7
blacki1035 <blacki1035@gmail.com>
2023-05-29 23:12:06 +0900

    CSRF_TRUSTED_ORIGINS 제거

--- soojango/settings.py
+++ soojango/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
 import environ
 import os
 
@@ -48,7 +47,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://']
+# CSRF_TRUSTED_ORIGINS = ['http://']
 # Application definition
 
 INSTALLED_APPS = [
